d47a9d7012ca257b469b42ec192a482f61c3bd5e
Petr Pavelka <petr.pavelka@student.gyarab.cz>
2023-11-02 21:12:25 +0100

    bomba.bum
    bomba.bum

--- hrac.py
+++ hrac.py
@@ -4,6 +4,9 @@ import mapa
 pygame.init()
 
 hrac_obr = pygame.image.load("images/ninja.png")
+bomba1_obr = pygame.image.load("images/bomb.png")
+bomba2_obr = pygame.image.load("images/bomb_red.png")
+exploze = pygame.image.load("images/explosion.png")
 
 class Player:
     def __init__(self):
@@ -14,6 +17,8 @@ class Player:
         self.policko_y = 1
         self.posledne = pygame.time.get_ticks()
         self.cooldown = 100
+        self.pocet_bomb = 1
+        self.bomby = []
     def souradnice_policka(self, velikost_policka, novy_x, novy_y):
         self.x = self.policko_x * velikost_policka + novy_x
         self.y = self.policko_y * velikost_policka + novy_y
@@ -36,5 +41,49 @@ class Player:
             elif klavesa[pygame.K_d] and self.policko_x < 7:
                 if mapa.mapa[self.policko_x + 1][self.policko_y] != 0 and mapa.mapa[self.policko_x + 1][self.policko_y] != 2:
                     self.policko_x += 1
+            elif klavesa[pygame.K_SPACE] and self.pocet_bomb > 0:
+                self.vytvor_bombu()
+                self.pocet_bomb -= 1
 
             self.posledne = nyni
+
+    def vytvor_bombu(self):
+        bomba_x = self.x
+        bomba_y = self.y
+        bomba = Bomba(bomba_x, bomba_y)
+        self.bomby.append(bomba)
+
+    def kontrola_bomb(self):
+        nyni = pygame.time.get_ticks()
+        k_odstraneni = []
+
+        for bomba in self.bomby:
+            if bomba.bum:
+                if nyni - bomba.pocatek >= 3 * bomba.zpozdeni:
+                    k_odstraneni.append(bomba)
+                    self.pocet_bomb += 1
+            else:
+                bomba.fajci()
+        for bomba in k_odstraneni:
+            self.bomby.remove(bomba)
+
+class Bomba:
+    def __init__(self, x, y):
+        self.obr = bomba1_obr
+        self.x = x
+        self.y = y
+        self.bum = False
+        self.zpozdeni = 1000
+        self.pocatek = pygame.time.get_ticks()
+
+    def fajci(self):
+        nyni = pygame.time.get_ticks()
+        if nyni - self.pocatek >= self.zpozdeni:
+            self.obr = bomba2_obr
+        if nyni - self.pocatek >= 2 * self.zpozdeni:
+            self.obr = exploze
+            self.bum = True
+
+
+    def draw(self, screen):
+        screen.blit(self.obr, (self.x + 28, self.y + 14))
--- main.py
+++ main.py
@@ -1,44 +1,30 @@
 import pygame
-
-import mapa, hrac
+from hrac import Player
+import mapa
 
 pygame.init()
-sirka = 1920
-vyska = 1080
-screen = pygame.display.set_mode((sirka, vyska))
+
+pygame.display.set_icon(pygame.image.load("images/bomb.png"))
 pygame.display.set_caption("Bomber")
-rozliseni = (800, 600)
-meritko = (
-    sirka / rozliseni[0],
-    vyska / rozliseni[1]
-)
-# upravení obázků naa potřebnou velikost
-velikost_policka = 256
-zed_obr2 = pygame.transform.scale(mapa.zed_obr, (int(velikost_policka * meritko[0]), int(velikost_policka * meritko[1])))
-trava_obr2 = pygame.transform.scale(mapa.trava_obr, (int(velikost_policka * meritko[0]), int(velikost_policka * meritko[1])))
-box_obr2 = pygame.transform.scale(mapa.box_obr, (int(velikost_policka * meritko[0]), int(velikost_policka * meritko[1])))
-#výpočet velikosti mapy
-mapa_sirka = len(mapa.mapa[0]) * velikost_policka * meritko[0]
-mapa_vyska = len(mapa.mapa) * velikost_policka * meritko[1]
 
-#výpočet nových souřadnic x,y
-novy_x = (sirka - mapa_sirka) / 2
-novy_y = (vyska - mapa_vyska) / 2
-Fajci = True
+sirka = 1920
+vyska = 1140
+screen = pygame.display.set_mode((sirka, vyska))
+velikost_pole = 128
 
-# hlavní loop hry
-while Fajci:
+novy_x = (sirka - len(mapa.mapa[0]) * velikost_pole) // 2
+novy_y = (vyska - len(mapa.mapa) * velikost_pole) // 2
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            Fajci = False
+h1 = Player()
 
+fajci = True
+while fajci:
 
-
-    for rada in range(len(mapa.mapa)):
-        for sloupec in range(len(mapa.mapa[0])):
-            x = rada * velikost_policka * meritko[0] + novy_x
-            y = sloupec * velikost_policka * meritko[1] + novy_y
+# průchod polem mapa a vykreslení jednotlivých polí reprezentované obrázky
+    for rada in range(len(mapa.mapa[0])):
+        for sloupec in range(len(mapa.mapa)):
+            x = rada * velikost_pole + novy_x
+            y = sloupec * velikost_pole + novy_y
             if mapa.mapa[rada][sloupec] == 0:
                 screen.blit(mapa.zed_obr, (x, y))
             elif mapa.mapa[rada][sloupec] == 1:
@@ -46,15 +32,18 @@ while Fajci:
             elif mapa.mapa[rada][sloupec] == 2:
                 screen.blit(mapa.box_obr,(x, y))
 
+    h1.pohyb()
+    h1.souradnice_policka(velikost_pole, novy_x, novy_y)
+    h1.kontrola_bomb()
+    screen.blit(h1.obr, (h1.x, h1.y))
+
+    for bomby in h1.bomby:
+        bomby.fajci()
+        bomby.draw(screen)
+
     for event in pygame.event.get():
-        if event.type == pygame.VIDEORESIZE:
-            sirka, vyska = event.dict['size']
-            scale_factor = (
-                sirka / rozliseni[0],
-                vyska / rozliseni[1]
-            )
-            # Update the screen surface with the new size
-            screen = pygame.display.set_mode((sirka, vyska), pygame.RESIZABLE)
+        if event.type == pygame.QUIT:
+            fajci = False
 
     pygame.display.flip()
 
