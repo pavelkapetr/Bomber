3fd86ec2718a83bfaa35ae68727a89c93d0c65b6
Petr Pavelka <petr.pavelka@student.gyarab.cz>
2023-11-06 06:35:53 +0100

    nefajci

--- hrac.py
+++ hrac.py
@@ -10,7 +10,7 @@ exploze = pygame.image.load("images/explosion.png")
 
 
 class Player:
-    def __init__(self):
+    def __init__(self, mapa):
         self.obr = hrac_obr
         self.x = 0
         self.y = 0
@@ -20,11 +20,14 @@ class Player:
         self.cooldown = 100
         self.pocet_bomb = 1
         self.bomby = []
-        self.dosah = 1
+        self.dosah = 3
+        self.bumbumbox = []
+        self.mapa = mapa
 
     def souradnice_policka(self, velikost_policka, novy_x, novy_y):
-        self.x = self.policko_x * velikost_policka + novy_x
-        self.y = self.policko_y * velikost_policka + novy_y
+        x = self.policko_x * velikost_policka + novy_x
+        y = self.policko_y * velikost_policka + novy_y
+        return (x, y)
 
     def pohyb(self):
         nyni = pygame.time.get_ticks()
@@ -53,7 +56,7 @@ class Player:
     def vytvor_bombu(self):
         bomba_x = self.x
         bomba_y = self.y
-        bomba = Bomba(bomba_x, bomba_y, self.dosah, self.policko_x, self.policko_y)
+        bomba = Bomba(bomba_x, bomba_y, self.dosah, self.policko_x, self.policko_y, self.mapa)
         self.bomby.append(bomba)
 
     def kontrola_bomb(self):
@@ -65,6 +68,14 @@ class Player:
                 if nyni - bomba.pocatek >= 3 * bomba.zpozdeni:
                     k_odstraneni.append(bomba)
                     self.pocet_bomb += 1
+                    for i in range(bomba.policko_x - bomba.dosah, bomba.policko_x + bomba.dosah + 1):
+                        for j in range(bomba.policko_y - bomba.dosah, bomba.policko_y + bomba.dosah + 1):
+                            if (
+                                    0 <= i < len(self.mapa) and 0 <= j < len(self.mapa[0])
+                                    and (i != bomba.policko_x or j != bomba.policko_y)
+                            ):
+                                if self.mapa[i][j] == 2 or self.mapa[i][j] == 1:
+                                    self.bumbumbox.append((i, j, nyni))
             else:
                 bomba.fajci()
         for bomba in k_odstraneni:
@@ -72,8 +83,8 @@ class Player:
 
 
 class Bomba(Player):
-    def __init__(self, x, y, dosah, policko_x, policko_y):
-        super().__init__()
+    def __init__(self, x, y, dosah, policko_x, policko_y, mapa):
+        super().__init__(mapa)
         self.obr = bomba1_obr
         self.x = x
         self.y = y
@@ -92,10 +103,69 @@ class Bomba(Player):
             self.obr = exploze
             self.bum = True
 
-    def vybuch(self):
+    def vybuch(self, mapa):
         if self.bum:
-            if mapa.mapa[self.policko_x + 1][self.policko_y] != 0 and mapa.mapa[self.policko_x + 1][self.policko_y] != 2:
+            self.zacatek_vybuchu = pygame.time.get_ticks()
 
+            for dx in range(1, self.dosah + 1):
+                i = self.policko_x + dx
+                if i >= len(mapa) or mapa[i][self.policko_y] == 0:
+                    break
+                if mapa[i][self.policko_y] == 2:
+                    mapa[i][self.policko_y] = 1
+                    break
+
+            for dx in range(-1, -self.dosah - 1, -1):
+                i = self.policko_x + dx
+                if i < 0 or mapa[i][self.policko_y] == 0:
+                    break
+                if mapa[i][self.policko_y] == 2:
+                    mapa[i][self.policko_y] = 1
+                    break
+
+            for dy in range(1, self.dosah + 1):
+                j = self.policko_y + dy
+                if j >= len(mapa[0]) or mapa[self.policko_x][j] == 0:
+                    break
+                if mapa[self.policko_x][j] == 2:
+                    mapa[self.policko_x][j] = 1
+                    break
+
+            for dy in range(-1, -self.dosah - 1, -1):
+                j = self.policko_y + dy
+                if j < 0 or mapa[self.policko_x][j] == 0:
+                    break
+                if mapa[self.policko_x][j] == 2:
+                    mapa[self.policko_x][j] = 1
+                    break
+
+    def pozice_exploze(self, velikost_pole, novy_x, novy_y):
+        souradnice = []
+        bomba_x = self.policko_x
+        bomba_y = self.policko_y
+        dosah = self.dosah
+
+        for i in range(dosah):
+            if bomba_y > 1:
+                up_x = bomba_x * velikost_pole + novy_x
+                up_y = (bomba_y - i) * velikost_pole + novy_x
+                souradnice.append((up_x, up_y, pygame.time.get_ticks()))
+            if bomba_y < 7:
+                down_x = bomba_x * velikost_pole + novy_x
+                down_y = (bomba_y + i) * velikost_pole + novy_y
+                souradnice.append((down_x, down_y, pygame.time.get_ticks()))
+            if bomba_x > 7:
+                right_x = (bomba_x + i) * velikost_pole + novy_x
+                rigth_y = bomba_y * velikost_pole + novy_y
+                souradnice.append((right_x, rigth_y, pygame.time.get_ticks()))
+            if bomba_x < 1:
+                left_x = (bomba_x - 1) * velikost_pole + novy_x
+                left_y = bomba_y * velikost_pole + novy_x
+                souradnice.append((left_x, left_y, pygame.time.get_ticks()))
+            return souradnice
 
     def draw(self, screen):
-        screen.blit(self.obr, (self.x + 28, self.y + 14))
+        if self.obr == bomba1_obr or self.obr == bomba2_obr:
+            screen.blit(self.obr, (self.x + 28, self.y + 14))
+        else:
+            screen.blit(self.obr, (self.x, self.y))
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import mapa
 pygame.init()
 
 pygame.display.set_icon(pygame.image.load("images/bomb.png"))
+exploze = pygame.image.load("images/explosion.png")
 pygame.display.set_caption("Bomber")
 
 sirka = 1920
@@ -15,12 +16,18 @@ velikost_pole = 128
 novy_x = (sirka - len(mapa.mapa[0]) * velikost_pole) // 2
 novy_y = (vyska - len(mapa.mapa) * velikost_pole) // 2
 
-h1 = Player()
+h1 = Player(mapa.mapa)
 
 fajci = True
 while fajci:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            fajci = False
 
-# průchod polem mapa a vykreslení jednotlivých polí reprezentované obrázky
+    #vyčištění plochy
+    screen.fill((0, 0, 0))
+
+    # projde mapu a vyakreslí jednotlivé prvky jako reprezentované obrázky
     for rada in range(len(mapa.mapa[0])):
         for sloupec in range(len(mapa.mapa)):
             x = rada * velikost_pole + novy_x
@@ -30,22 +37,27 @@ while fajci:
             elif mapa.mapa[rada][sloupec] == 1:
                 screen.blit(mapa.trava_obr, (x, y))
             elif mapa.mapa[rada][sloupec] == 2:
-                screen.blit(mapa.box_obr,(x, y))
+                screen.blit(mapa.box_obr, (x, y))
 
-    h1.pohyb()
-    h1.souradnice_policka(velikost_pole, novy_x, novy_y)
-    h1.kontrola_bomb()
-    screen.blit(h1.obr, (h1.x, h1.y))
-
+    # projde všechny bomby a upraví jejich stav
     for bomby in h1.bomby:
-        bomby.fajci()
-        bomby.draw(screen)
         if bomby.bum:
             bomby.vybuch(mapa.mapa)
+            souradky_exploze = bomby.pozice_exploze(velikost_pole, novy_x, novy_y)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            fajci = False
+            # Draw explosion images
+            for x, y, cas_exploze in souradky_exploze:
+                screen.blit(exploze, (x, y))
+
+
+        # Call the remaining functions for bombs
+        bomby.fajci()
+        bomby.draw(screen)
+
+    h1.pohyb()
+    h1.x, h1.y = h1.souradnice_policka(velikost_pole, novy_x, novy_y)
+    h1.kontrola_bomb()
+    screen.blit(h1.obr, (h1.x, h1.y))
 
     pygame.display.flip()
 
